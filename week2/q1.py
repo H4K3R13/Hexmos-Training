dd6648a7fa6ee7e2090a4be9cffbc5cd68f7e250
H4K3R13 <arjunmundamani@gmail.com>
2023-05-18 21:02:55 +0530

    Week 2 Finish ⭐

--- week2/q1.py
+++ week2/q1.py
@@ -1,3 +1,4 @@
+import json
 # Q1 line 1 to 24 
 def load_fps(filepath):
     with open(filepath, 'r') as file:
@@ -34,7 +35,7 @@ def filter_by_tags(polls_data, list_of_tags):
                     filtered_data.append(poll)
     return filtered_data
 
-#Q3 line 37 to 
+#Q3 line 37 to 47
 
 def view_poll(poll_data, pollNumber):
     item = poll_data[pollNumber]
@@ -46,22 +47,44 @@ def view_poll(poll_data, pollNumber):
     for tag in item['Tags']:
         print(tag, end=' ')
     
+#Q4 line 49 to 58
+def update_poll(poll_data, pollNumber, option):
+    #print(poll_data)
+    options = poll_data[pollNumber]['Options']
+    #print(options)
+    votes = poll_data[pollNumber]['Votes']
+    #print(votes)
+    index = options.index(option+' ')
+    votes[index] = str(int(votes[index].strip()) + 1)
+    return poll_data
 
-
-
+#Q5 line 60
+def save_poll(poll_data, filename):
+   with open(filename, 'w') as file:
+        json.dump(poll_data, file)
 
 if __name__ == "__main__":
     filepath = "./polldata.fps"
     print("\nQ1 : Making List of dictionary")
     data = load_fps(filepath)
-    #for x in data:
-       # print("\n",x)
+    for x in data:
+       print("\n",x)
 
     print("\n Q2 : Output of filtered data by tag")
     data2 = filter_by_tags(data, ['phones', 'cricket'])
-    #for x in data2:
-        #print("\n",x)
+    for x in data2:
+        print("\n",x)
 
     print("\n Q3 : Printing the question and poll results")
     qs_no = int(input("Enter the Question Number"))
     view_poll(data,qs_no)
+
+    print("\n Q4 : Voting the option")
+    updated_data = update_poll(data,1,'Yes')
+    print(updated_data)
+
+    print("\n Q5 : Saving the update_data to update_polldata.fps")
+    save_poll(updated_data,"updated_poll.json")
+
+
+

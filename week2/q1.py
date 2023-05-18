02f802bbfe6e503b20a3d6f6b831321ac8959eab
H4K3R13 <arjunmundamani@gmail.com>
2023-05-18 19:57:05 +0530

    Complete Q3

--- week2/q1.py
+++ week2/q1.py
@@ -1,4 +1,4 @@
-# line 1 to 24 Q1
+# Q1 line 1 to 24 
 def load_fps(filepath):
     with open(filepath, 'r') as file:
         lines = file.readlines()[1:]
@@ -23,7 +23,7 @@ def load_fps(filepath):
 
     return(result)
 
-# line 26 Q2
+# Q2 line 26 to 35 
 def filter_by_tags(polls_data, list_of_tags):
     filtered_data = []
     for poll in polls_data:
@@ -34,19 +34,34 @@ def filter_by_tags(polls_data, list_of_tags):
                     filtered_data.append(poll)
     return filtered_data
 
+#Q3 line 37 to 
+
+def view_poll(poll_data, pollNumber):
+    item = poll_data[pollNumber]
+    print("\n",item['Question'])
+    options,votes = item['Options'],item['Votes'] #Deconstruting 
+    for option,vote in zip(options,votes):
+            print('\n' + option + ':' + vote)
+    print("\nTags: ")
+    for tag in item['Tags']:
+        print(tag, end=' ')
+    
+
+
 
 
 if __name__ == "__main__":
     filepath = "./polldata.fps"
+    print("\nQ1 : Making List of dictionary")
     data = load_fps(filepath)
+    #for x in data:
+       # print("\n",x)
 
-    #print(data)
-
-    for x in data:
-        print("\n",x)
-
-    print("\n Output of filtered data by tag")
+    print("\n Q2 : Output of filtered data by tag")
     data2 = filter_by_tags(data, ['phones', 'cricket'])
-    #print(data2)
-    for x in data2:
-        print("\n",x)
+    #for x in data2:
+        #print("\n",x)
+
+    print("\n Q3 : Printing the question and poll results")
+    qs_no = int(input("Enter the Question Number"))
+    view_poll(data,qs_no)

9edc94d73d325a1d2611664a2dcd1fa9429671a5
H4K3R13 <arjunmundamani@gmail.com>
2023-05-19 09:20:59 +0530

    Finishing Up Week2

--- week2/q1.py
+++ week2/q1.py
@@ -14,12 +14,10 @@ def load_fps(filepath):
         tag = line[3].strip().split("|")
     
         item = {
-            "Question" : question,
-            "Options"  : options,
-            "Votes"    : votes,
-            "Tags"     : tag
+            "Question": question,
+            "OptionVote": dict(zip(options, votes)),
+            "Tags": tag
         }
-
         result.append(item)
 
     return(result)
@@ -40,22 +38,21 @@ def filter_by_tags(polls_data, list_of_tags):
 def view_poll(poll_data, pollNumber):
     item = poll_data[pollNumber]
     print("\n",item['Question'])
-    options,votes = item['Options'],item['Votes'] #Deconstruting 
-    for option,vote in zip(options,votes):
-            print('\n' + option + ':' + vote)
-    print("\nTags: ")
+    print(item)
+    ov = item['OptionVote'] 
+    for option, vote in ov.items():
+        print(option + ':' + vote)
+
     for tag in item['Tags']:
         print(tag, end=' ')
     
 #Q4 line 49 to 58
 def update_poll(poll_data, pollNumber, option):
     #print(poll_data)
-    options = poll_data[pollNumber]['Options']
-    #print(options)
-    votes = poll_data[pollNumber]['Votes']
-    #print(votes)
-    index = options.index(option+' ')
-    votes[index] = str(int(votes[index].strip()) + 1)
+    ov = poll_data[pollNumber]['OptionVote']
+    #print(ov[option])
+    ov[option] = str(int(ov[option]) + 1)
+    #print(ov)
     return poll_data
 
 #Q5 line 60
@@ -67,8 +64,9 @@ if __name__ == "__main__":
     filepath = "./polldata.fps"
     print("\nQ1 : Making List of dictionary")
     data = load_fps(filepath)
-    for x in data:
-       print("\n",x)
+    print(data)
+    #for x in data:
+       #print("\n",x)
 
     print("\n Q2 : Output of filtered data by tag")
     data2 = filter_by_tags(data, ['phones', 'cricket'])
@@ -80,7 +78,7 @@ if __name__ == "__main__":
     view_poll(data,qs_no)
 
     print("\n Q4 : Voting the option")
-    updated_data = update_poll(data,1,'Yes')
+    updated_data = update_poll(data,1,'Yes ')
     print(updated_data)
 
     print("\n Q5 : Saving the update_data to update_polldata.fps")

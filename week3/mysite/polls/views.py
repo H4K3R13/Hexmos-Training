9304ece4e880ecaea636d15307383efd798f5716
H4K3R13 <arjunmundamani@gmail.com>
2023-05-20 21:29:54 +0530

    Django html view

--- week3/mysite/polls/views.py
+++ week3/mysite/polls/views.py
@@ -1,4 +1,18 @@
 from django.http import HttpResponse
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello World this is my first django app.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = " , ". join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at the question %s" % question_id)
+
+def results(request, question_id):
+    response = "You are looking at the result of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s" %question_id)

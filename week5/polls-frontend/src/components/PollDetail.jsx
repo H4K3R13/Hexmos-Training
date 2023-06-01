deaff592f182ee2465452993fca6fb9ff8f197dd
H4K3R13 <arjunmundamani@gmail.com>
2023-06-01 19:47:39 +0530

    Added Fun. to vote a option 🥧

--- week5/polls-frontend/src/components/PollDetail.jsx
+++ week5/polls-frontend/src/components/PollDetail.jsx
@@ -17,52 +17,66 @@ function PollDetail() {
         console.log(response.data)
         setPollsData(response.data)
     }
-     catch(error){
+    catch(error){
         console.log(error)
-     }
+    }
   }
-    //Fix Chart
-    const piedata = [["Options", "Votes"]];
-    pollsData.map((poll)=>{
-      Object.entries(poll.OptionVote).map(([option, vote])=>{
-        //const opvot = [...piedata]
-        piedata.push([option,vote])
-        //console.log(piedata)
-      })
-    })
-    const options = {
-      title: "Polls Votes",
-    };
 
-    console.log("piedata",piedata)
+  const handleVote = async (pollId, option) => {
+    try {
+      const response = await axios.put(`http://127.0.0.1:8000/polls/api/polls_vote/${pollId}/`, {
+        incrementOption: option
+      });
+      console.log(response.data);
+      alert(response.data["message"])
+      fetchData()
+      // Update the polls data or display a success message
+    } catch (error) {
+      console.log(error);
+      // Handle the error response or display an error message
+    }
+  };
+
+  // Fix Chart
+  const piedata = [["Options", "Votes"]];
+  pollsData.forEach((poll) => {
+    Object.entries(poll.OptionVote).forEach(([option, vote]) => {
+      piedata.push([option, vote]);
+    });
+  });
+  const options = {
+    title: "Polls Votes",
+  };
+
+  console.log("piedata", piedata);
 
   return (
-    <div>
-        <h1 style={{ textAlign:"center"}}>PollDetail: {id}</h1>
-        {pollsData.map((poll,index) => (
-            <div key={index} style={{ fontSize:"1.8rem"}}>
-                <h2>{poll.Question}</h2>
-                <ul>
-                    {Object.entries(poll.OptionVote).map(([option, votes]) => (
-                        <li key={option}>
-                        {option}: {votes}
-                        </li>
-                    ))}
-                </ul>
-                <p>Tags: {poll.Tags.join(", ")}</p>
-            </div>
-        ))}
-        <div style={{ width:"35rem", height:"35rem"}}>
-          <Chart
-            chartType="PieChart"
-            data={piedata}
-            options={options}
-            width={"100%"}
-            height={"100%"}
-            />
+    <div style={{ display:'flex', flexDirection:"row", gap:"5rem"}}>
+      <h1 style={{ textAlign:"center"}}>PollDetail: {id}</h1>
+      {pollsData.map((poll, index) => (
+        <div key={index} style={{ fontSize:"1.8rem", marginTop:"2rem"}}>
+          <h2>{poll.Question}</h2>
+          <ul>
+            {Object.entries(poll.OptionVote).map(([option, votes]) => (
+              <li key={option}>
+                {option}: {votes}
+                <button onClick={() => handleVote(id, option)} style={{ padding:".2rem .8rem", marginLeft:".2rem", background:"yellow", borderRadius:"10px"}}>Vote</button>
+              </li>
+            ))}
+          </ul>
+          <p>Tags: {poll.Tags.join(", ")}</p>
         </div>
+      ))}
+      <div style={{ width:"35rem", height:"35rem"}}>
+        <Chart
+          chartType="PieChart"
+          data={piedata}
+          options={options}
+          width={"100%"}
+          height={"100%"}
+        />
+      </div>
     </div>
-
   )
 }
 
